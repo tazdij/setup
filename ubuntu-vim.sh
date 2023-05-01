96f6efd22d4f71efc3cbd982db5fcb6670f23ad1
Donald Duvall <shout@tazdij.com>
2023-05-01 03:56:16 -0700

    Using installer from tazdij fork of dein.vim

--- ubuntu-vim.sh
+++ ubuntu-vim.sh
@@ -10,9 +10,8 @@
 sudo apt update && sudo apt install -y git vim curl
 
 # We need to obtain and run the dein install
-#curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh  # OLD Install from main (Main has new install procedure)
-#curl https://raw.githubusercontent.com/Shougo/dein.vim/release/bin/installer.sh > installer.sh
-curl https://raw.githubusercontent.com/Shougo/dein-installer.vim/main/installer.sh > installer.sh
+# We now install from our own fork of dein (to continue using the older version, and installer)
+curl https://raw.githubusercontent.com/tazdij/dein.vim/2.2/bin/installer.sh > installer.sh
 
 # Run installer and clean installer file
 sh ./installer.sh ~/.cache/dein; rm ./installer.sh
